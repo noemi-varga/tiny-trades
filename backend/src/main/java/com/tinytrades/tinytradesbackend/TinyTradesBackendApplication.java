87ce65b034c062f0569298498590470c315a6ad2
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-14 14:02:45 +0100

    initialize Spring Boot project

---
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/TinyTradesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tinytrades.tinytradesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyTradesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyTradesBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tinytrades/tinytradesbackend/TinyTradesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinytrades.tinytradesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTradesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
