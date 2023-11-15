761330d78265d4b091aa1fa40f5ad254978cb12f
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-15 23:23:29 +0100

    create product abstract entity class

---
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/model/product/Product.java
@@ -0,0 +1,75 @@
+package com.tinytrades.tinytradesbackend.model.product;
+
+import com.tinytrades.tinytradesbackend.model.User;
+import com.tinytrades.tinytradesbackend.model.enums.AgeGroup;
+import com.tinytrades.tinytradesbackend.model.enums.ConditionType;
+import com.tinytrades.tinytradesbackend.model.enums.Gender;
+import com.tinytrades.tinytradesbackend.model.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "products")
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trader_id", referencedColumnName = "id")
+    //Try later: @CreatedBy
+    private User trader;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "condition")
+    private ConditionType condition;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "age_group")
+    private AgeGroup ageGroup;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "product_tags", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "tag")
+    @Builder.Default
+    private Set<String> tags = new HashSet<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+
+}
