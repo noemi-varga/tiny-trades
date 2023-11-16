59cf4bf3c72b9f1c88df432149496187b41cbe5e
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-16 15:18:10 +0100

    implement a UserMapper for mapping UserResponses and Users

---
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package com.tinytrades.tinytradesbackend.mapper;
+
+import com.tinytrades.tinytradesbackend.dto.user.NewUserRequest;
+import com.tinytrades.tinytradesbackend.dto.user.UserResponse;
+import com.tinytrades.tinytradesbackend.model.User;
+
+public class UserMapper {
+
+    public static UserResponse mapToUserResponse(User user) {
+        return UserResponse
+                .builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .build();
+    }
+
+    public static User mapToUser(NewUserRequest newUserRequest) {
+        return User
+                .builder()
+                .username(newUserRequest.username())
+                .firstName(newUserRequest.firstName())
+                .lastName(newUserRequest.lastName())
+                .email(newUserRequest.email())
+                .password(newUserRequest.password())
+                .phoneNumber(newUserRequest.phoneNumber())
+                .location(newUserRequest.location())
+                .build();
+    }
+}
