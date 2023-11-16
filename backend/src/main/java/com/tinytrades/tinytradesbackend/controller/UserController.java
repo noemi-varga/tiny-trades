8466c4d581c6929fb6749ee54705b055dd4cc5b6
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-16 15:39:59 +0100

    create endpoint for updating user by id

--- backend/src/main/java/com/tinytrades/tinytradesbackend/controller/UserController.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.tinytrades.tinytradesbackend.controller;
 
+import com.tinytrades.tinytradesbackend.dto.user.UpdateUserRequest;
 import com.tinytrades.tinytradesbackend.dto.user.NewUserRequest;
 import com.tinytrades.tinytradesbackend.dto.user.UserResponse;
 import com.tinytrades.tinytradesbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +34,12 @@ public class UserController {
     @PostMapping("/users/")
     public UserResponse addUser(@RequestBody NewUserRequest newUserRequest) {
         return userService.addUser(newUserRequest);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<?> updateUserById(@PathVariable Long id, @RequestBody UpdateUserRequest updateUserRequest) {
+        UserResponse userResponse = userService.updateUserById(id, updateUserRequest);
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 
 
