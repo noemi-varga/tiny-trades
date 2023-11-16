a870ed9892d58390005942373936a3d7dbedbabe
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-16 15:39:26 +0100

    implement updateUserById method

--- backend/src/main/java/com/tinytrades/tinytradesbackend/service/UserService.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/service/UserService.java
@@ -1,6 +1,7 @@
 package com.tinytrades.tinytradesbackend.service;
 
 import com.tinytrades.tinytradesbackend.dto.user.NewUserRequest;
+import com.tinytrades.tinytradesbackend.dto.user.UpdateUserRequest;
 import com.tinytrades.tinytradesbackend.dto.user.UserResponse;
 import com.tinytrades.tinytradesbackend.mapper.UserMapper;
 import com.tinytrades.tinytradesbackend.model.User;
@@ -24,13 +25,32 @@ public class UserService {
         return users.stream().map(UserMapper::mapToUserResponse).toList();
     }
 
+    private User findUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("User not found with id: " + id));
+    }
+
     public UserResponse findUserResponseById(Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("User not found with id: " + id));
-        return UserMapper.mapToUserResponse(user);
+        return UserMapper.mapToUserResponse(findUserById(id));
     }
 
     public UserResponse addUser(NewUserRequest newUserRequest) {
         User user = userRepository.save(UserMapper.mapToUser(newUserRequest));
         return UserMapper.mapToUserResponse(user);
     }
+
+    public UserResponse updateUserById(Long id, UpdateUserRequest updateUserRequest) {
+        User userToUpdate = findUserById(id);
+
+        userToUpdate.setUsername(updateUserRequest.username());
+        userToUpdate.setFirstName(updateUserRequest.firstName());
+        userToUpdate.setLastName(updateUserRequest.lastName());
+        userToUpdate.setEmail(updateUserRequest.email());
+        userToUpdate.setPassword(updateUserRequest.password());
+        userToUpdate.setLocation(updateUserRequest.location());
+        userToUpdate.setPhoneNumber(userToUpdate.getPhoneNumber());
+
+        User user = userRepository.save(userToUpdate);
+        return UserMapper.mapToUserResponse(user);
+    }
+
 }
