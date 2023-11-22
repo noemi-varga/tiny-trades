3e763c894b2dcfaa59e3048b28d8bde3e048151e
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-22 17:59:47 +0100

    implement adding new Clothing

--- backend/src/main/java/com/tinytrades/tinytradesbackend/controller/ClothingController.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/controller/ClothingController.java
@@ -2,6 +2,7 @@ package com.tinytrades.tinytradesbackend.controller;
 
 import com.tinytrades.tinytradesbackend.dto.product.ProductResponse;
 import com.tinytrades.tinytradesbackend.dto.product.clothing.ClothingResponse;
+import com.tinytrades.tinytradesbackend.dto.product.clothing.NewClothing;
 import com.tinytrades.tinytradesbackend.service.ClothingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,11 @@ public class ClothingController {
     @GetMapping("/clothing/")
     public List<ProductResponse> findAllClothing() {
         return clothingService.findAllClothing();
+    }
+
+    @PostMapping("/users/{userId}/clothing/")
+    public ProductResponse addNewClothing(@PathVariable Long userId, @RequestBody NewClothing newClothing){
+        return clothingService.addNewClothing(userId, newClothing);
     }
 
 
--- backend/src/main/java/com/tinytrades/tinytradesbackend/service/ClothingService.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/service/ClothingService.java
@@ -2,26 +2,55 @@ package com.tinytrades.tinytradesbackend.service;
 
 import com.tinytrades.tinytradesbackend.dto.product.ProductResponse;
 import com.tinytrades.tinytradesbackend.dto.product.clothing.ClothingResponse;
+import com.tinytrades.tinytradesbackend.dto.product.clothing.NewClothing;
 import com.tinytrades.tinytradesbackend.mapper.ProductMapper;
+import com.tinytrades.tinytradesbackend.model.User;
+import com.tinytrades.tinytradesbackend.model.enums.*;
 import com.tinytrades.tinytradesbackend.model.product.Clothing;
 import com.tinytrades.tinytradesbackend.repository.ClothingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class ClothingService {
 
     private final ClothingRepository clothingRepository;
+    private final UserService userService;
 
     @Autowired
-    public ClothingService(ClothingRepository clothingRepository) {
+    public ClothingService(ClothingRepository clothingRepository, UserService userService) {
         this.clothingRepository = clothingRepository;
+        this.userService = userService;
     }
 
     public List<ProductResponse> findAllClothing() {
         List<Clothing> clothing = clothingRepository.findAll();
         return clothing.stream().map(ProductMapper::mapToProductResponse).toList();
+    }
+
+    public ProductResponse addNewClothing(Long userId, NewClothing newClothing) {
+        User user = userService.findUserById(userId);
+        Clothing clothing = Clothing.builder()
+                .trader(user)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .title(newClothing.title())
+                .gender(Gender.valueOf(newClothing.gender()))
+                .condition(ConditionType.valueOf(newClothing.condition()))
+                .ageGroup(AgeGroup.valueOf(newClothing.ageGroup()))
+                .description(newClothing.description())
+                .tags(newClothing.tags())
+                .status(Status.ACTIVE)
+                .size(ClothingSize.valueOf(newClothing.size()))
+                .color(ClothingColor.valueOf(newClothing.color()))
+                .clothingCategory(ClothingCategory.valueOf(newClothing.clothingCategory()))
+                .build();
+
+        Clothing savedClothing = clothingRepository.save(clothing);
+
+        return ProductMapper.mapToProductResponse(savedClothing);
     }
 }
