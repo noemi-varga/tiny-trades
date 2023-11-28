16f9252cab4a5bed672cfa80a424e1678424f504
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-28 13:50:06 +0100

    create ProductItem component

---
+++ frontend/src/Components/ProductItem/ProductItem.jsx
@@ -0,0 +1,11 @@
+const ProductItem = ({ item }) => {
+
+    return (
+        <div className="product-item">
+            <p>{item.title}</p>
+      </div>
+    );
+
+}
+
+export default ProductItem;
