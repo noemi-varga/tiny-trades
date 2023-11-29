60477ee18c62d1d9ab72fac457fc7d8fa6062984
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-29 09:50:46 +0100

    implement navigation to product page

--- frontend/src/Components/ProductItem/ProductItem.jsx
+++ frontend/src/Components/ProductItem/ProductItem.jsx
@@ -1,8 +1,10 @@
-const ProductItem = ({ item }) => {
+const ProductItem = ({ item, onOpen }) => {
 
     return (
         <div className="product-item">
             <p>{item.title}</p>
+            <p>Type: {item.productType}</p>
+            <button onClick={() => onOpen(item.id, item.productType)}>Open</button>
       </div>
     );
 
--- frontend/src/Components/ProductList/ProductList.jsx
+++ frontend/src/Components/ProductList/ProductList.jsx
@@ -1,14 +1,21 @@
 import ProductItem from "../ProductItem";
 import "./ProductList.css"
+import { useNavigate } from "react-router-dom";
 
 const ProductList = ({ products }) => {
+
+    const navigate = useNavigate();
+
+    const handleRedirection = (id, type) => {
+      navigate(`/product/${id}`, { state: { productType: type } });
+    };
 
     return (
         <div className="product-list">
         <h2>All products</h2>
         <h4>{products.length} products total</h4>
           {products.map((item, index) => (
-            <ProductItem key={index} item={item}/>
+            <ProductItem key={index} item={item} onOpen={handleRedirection}/>
           ))}
       </div>
     );
