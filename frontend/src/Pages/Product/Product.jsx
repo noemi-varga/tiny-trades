aa4255ed7eb3364ccb9e5395801d0b325590e394
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-11-30 14:48:58 +0100

    style Product page

--- frontend/src/Pages/Product/Product.jsx
+++ frontend/src/Pages/Product/Product.jsx
@@ -6,8 +6,8 @@ import { useLocation } from "react-router-dom";
 import "./Product.css";
 
 const Product = () => {
-const location = useLocation();
-const { productType } = location.state;
+  const location = useLocation();
+  const { productType } = location.state;
   const { productId } = useParams();
   const [product, setProduct] = useState([]);
 
@@ -29,19 +29,30 @@ const { productType } = location.state;
   }, [productType, productId]);
 
   return (
-    <>
-      <p>{product.title}</p>
-      {product.gender && <p>Gender: {product.gender}</p>}
-      {product.condition && <p>Condition: {product.condition}</p>}
-      {product.ageGroup && <p>Age Group: {product.ageGroup}</p>}
-      {product.description && <p>Description: {product.description}</p>}
-      {product.tags && <p>Tags: {product.tags.join(', ')}</p>}
-      {product.status && <p>Status: {product.status}</p>}
-      {product.size && <p>Size: {product.size}</p>}
-      {product.color && <p>Color: {product.color}</p>}
-      {product.clothingCategory && <p>Clothing Category: {product.clothingCategory}</p>}
-      {product.toyCategory && <p>Toy Category: {product.toyCategory}</p>}
-    </>
+    <div className="product-container">
+      <div className="product-images">
+        {product.imageLinks?.map((image, index) => (
+          <img key={index} src={image} alt={`${product.title} - ${index}`} />
+        ))}
+      </div>
+      <div className="product-title">
+      <h3>{product.title}</h3>
+      </div>
+      <div className="product-details">
+        {product.gender && <p>Gender: {product.gender}</p>}
+        {product.condition && <p>Condition: {product.condition}</p>}
+        {product.ageGroup && <p>Age Group: {product.ageGroup}</p>}
+        {product.description && <p>Description: {product.description}</p>}
+        {product.tags && <p>Tags: {product.tags.join(", ")}</p>}
+        {product.status && <p>Status: {product.status}</p>}
+        {product.size && <p>Size: {product.size}</p>}
+        {product.color && <p>Color: {product.color}</p>}
+        {product.clothingCategory && (
+          <p>Clothing Category: {product.clothingCategory}</p>
+        )}
+        {product.toyCategory && <p>Toy Category: {product.toyCategory}</p>}
+      </div>
+    </div>
   );
 };
 
