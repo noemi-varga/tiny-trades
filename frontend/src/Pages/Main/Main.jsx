767135ad2b69e303d129a3278504dc39edc66149
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-12-01 11:45:42 +0100

    add ProductUpload button

--- frontend/src/Pages/Main/Main.jsx
+++ frontend/src/Pages/Main/Main.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import "./Main.css"
-import ProductList from '../../Components/ProductList';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Main.css";
+import ProductList from "../../Components/ProductList";
 
 const Main = () => {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-const fetchProducts = () => {
-  fetch(`/api/v1/products`)
-  .then((res) => res.json())
-  .then((result) => {
-    setProducts(result);
-  })
-  .catch((error) =>
-  console.log(`An error occurred at fetching from /api/v1/products:${error}`)
-);
-}
+  const handleAddItemClick = () => {
+    navigate("/product/new");
+  };
+
+  const fetchProducts = () => {
+    fetch(`/api/v1/products`)
+      .then((res) => res.json())
+      .then((result) => {
+        setProducts(result);
+      })
+      .catch((error) =>
+        console.log(
+          `An error occurred at fetching from /api/v1/products:${error}`
+        )
+      );
+  };
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
-   <>
-   <ProductList products={products}/>
-   </>
+    <div className="main-page">
+      <button className="upload-button" onClick={() => handleAddItemClick()}>+ Add Your Item</button>
+      <ProductList products={products} />
+    </div>
   );
-}
+};
 
 export default Main;
