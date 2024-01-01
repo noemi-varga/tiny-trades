836e36e3e15f3266427af63a367da9fd2f333b65
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2024-01-01 18:04:28 +0100

    create ProductRepository

---
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.tinytrades.tinytradesbackend.repository;
+
+import com.tinytrades.tinytradesbackend.model.product.Clothing;
+import com.tinytrades.tinytradesbackend.model.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Clothing> {
+
+}
