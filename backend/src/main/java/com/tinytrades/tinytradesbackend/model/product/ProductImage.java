606ae88fb8cf4bb316e7851e2bb9a317dd9f76d7
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2024-01-01 17:47:29 +0100

    remove unnecessary join columns

--- backend/src/main/java/com/tinytrades/tinytradesbackend/model/product/ProductImage.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/model/product/ProductImage.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="product_images")
+@Table
 public class ProductImage {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -18,11 +18,4 @@ public class ProductImage {
 
     private String url;
 
-    @ManyToOne
-    @JoinColumn(name = "clothing_id", nullable = true)
-    private Clothing clothing;
-
-    @ManyToOne
-    @JoinColumn(name = "toy_id", nullable = true)
-    private Toy toy;
 }
