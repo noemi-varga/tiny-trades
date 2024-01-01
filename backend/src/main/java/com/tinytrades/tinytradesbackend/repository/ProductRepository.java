835e3a47fe31c93a398b566345e4dae2d525ffb2
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2024-01-01 18:32:48 +0100

    implement finding all products by user id

--- backend/src/main/java/com/tinytrades/tinytradesbackend/repository/ProductRepository.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.tinytrades.tinytradesbackend.repository;
 
 import com.tinytrades.tinytradesbackend.model.product.Clothing;
 import com.tinytrades.tinytradesbackend.model.product.Product;
+import com.tinytrades.tinytradesbackend.model.product.Toy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Clothing> {
 
+    List<Product> findAllByTrader_IdIs(Long userId);
 }
--- backend/src/main/java/com/tinytrades/tinytradesbackend/service/ProductService.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/service/ProductService.java
@@ -2,7 +2,6 @@ package com.tinytrades.tinytradesbackend.service;
 
 import com.tinytrades.tinytradesbackend.dto.product.ProductResponse;
 import com.tinytrades.tinytradesbackend.mapper.ProductMapper;
-import com.tinytrades.tinytradesbackend.model.product.Clothing;
 import com.tinytrades.tinytradesbackend.model.product.Product;
 import com.tinytrades.tinytradesbackend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +19,13 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<ProductResponse> findAllProduct() {
+    public List<ProductResponse> findAllProducts() {
         List<Product> products = productRepository.findAll();
+        return products.stream().map(ProductMapper::mapToProductResponse).toList();
+    }
+
+    public List<ProductResponse> findAllProductsByUserId(Long userId) {
+        List<Product> products = productRepository.findAllByTrader_IdIs(userId);
         return products.stream().map(ProductMapper::mapToProductResponse).toList();
     }
 
