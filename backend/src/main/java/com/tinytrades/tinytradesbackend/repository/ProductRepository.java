2619ee0fb03d76e5bebb375599119d789ce90eb3
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2024-01-03 18:31:40 +0100

    fix typo

--- backend/src/main/java/com/tinytrades/tinytradesbackend/repository/ProductRepository.java
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/repository/ProductRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Clothing> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByTrader_IdIs(Long userId);
 }
