d52609f069025d6df725cfb7e560e052b533f86c
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2024-01-11 14:20:04 +0100

    create Token entity

---
+++ backend/src/main/java/com/tinytrades/tinytradesbackend/security/model/Token.java
@@ -0,0 +1,31 @@
+package com.tinytrades.tinytradesbackend.security.model;
+
+import com.tinytrades.tinytradesbackend.model.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @Column(unique = true)
+    public String token;
+
+    public String tokenType = "Bearer";
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
+}
